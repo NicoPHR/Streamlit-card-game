169ae6d0de21b165fd9bd0408dbfb738044dfee6
NicoPHR <nphillipsrossi@gmail.com>
2025-06-11 21:52:53 -0300

    Update test.py

--- test.py
+++ test.py
@@ -27,7 +27,6 @@ st.divider()
 st.image(os.path.join(os.getcwd(), "static", "mari.png"), width=100)
 
 choice = st.radio("Wählen Sie ihre sprache", ["Englisch auf Deutsch", "Deutsch auf Englisch"])
-print(choice)
 
 # Next card resets both the word and hides the answer again
 if st.button("Neues Wort", key="next"):
@@ -45,15 +44,9 @@ if st.button("Antwort anzeigen", key="show"):
     else:
         st.subheader(f"Englisch Wort: {st.session_state.random_word['translation']}")
 
-
-
-
-
 # Only render the translation if that flag is True
 if st.session_state.show_answer:
     st.write(f"Englisches Wort: {st.session_state.random_word['translation']}")
-
-
 
 st.session_state.show_answer = False
 
